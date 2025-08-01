dfa4708530f40cba0329d95cdce80556f013940a
nug31 <nugrohodc06@gmail.com>
2025-08-01 10:41:15 +0700

    Add simple test function for debugging

---
+++ netlify/functions/test.js
@@ -0,0 +1,30 @@
+exports.handler = async (event, context) => {
+  try {
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+      },
+      body: JSON.stringify({
+        message: 'Test function working!',
+        timestamp: new Date().toISOString(),
+        event: event.httpMethod + ' ' + event.path
+      })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        error: 'Function error',
+        message: error.message
+      })
+    };
+  }
+};
